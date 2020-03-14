8cca63c7700b388fafa0465109d0260941c4eb0b
Maarten <maarten.vanalebeek@experius.nl>
2020-03-14 15:38:50 +0100

    permission requesting works

--- app/src/main/java/com/example/androidassessment/MainActivity.java
+++ app/src/main/java/com/example/androidassessment/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.androidassessment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.androidassessment.dummy.DummyContent;
 
@@ -12,6 +17,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int READ_CONTACT_PERMISSION_REQUEST = 1;
+    boolean READ_CONTACT_PERMISSION_GRANTED = false;
 
     @Override
     // - Startup splash screen
@@ -24,7 +32,46 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         List<Contact> list = new ArrayList<>();
 
-        // Add contacts from phone
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS}, READ_CONTACT_PERMISSION_REQUEST);
+        }
+
+        if(READ_CONTACT_PERMISSION_GRANTED)
+        {
+            // Read Contacts
+            readContacts();
+
+        }
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch(requestCode)
+        {
+            case READ_CONTACT_PERMISSION_REQUEST:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show();
+                    READ_CONTACT_PERMISSION_GRANTED = true;
+                }
+            }
+        }
+    }
+
+    private void readContacts(){
+        
+
+
+
+
 
     }
 }
